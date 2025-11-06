ad583b1cc0e69c2d8a236e8a8ced7a4a6ccf8810
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-06 22:52:25 +0000

    Initial routing of the project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: 50 }}>
+      <h1>Login Page</h1>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
