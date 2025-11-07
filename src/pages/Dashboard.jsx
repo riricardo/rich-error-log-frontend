d9c1fd076bd714282d8f0283ba6d6d64487843a7
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-07 01:47:38 +0000

    dashboard v1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,53 @@
 const Dashboard = () => {
-  return <>dashboard</>;
+  return (
+    <>
+      <select defaultValue="All" className="select m-4">
+        <option>All</option>
+        <option>App1</option>
+        <option>App2</option>
+      </select>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="join flex justify-center m-4">
+        <button className="join-item btn">1</button>
+        <button className="join-item btn">2</button>
+        <button className="join-item btn btn-disabled">...</button>
+        <button className="join-item btn">99</button>
+        <button className="join-item btn">100</button>
+      </div>
+    </>
+  );
 };
 export default Dashboard;
