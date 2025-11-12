7d9c0d0a589bf5fda73c2c37cc38073624b5253e
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-12 04:29:44 +0000

    highlight header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,25 @@
-import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContextValue";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const { logout } = useAuth();
 
   return (
-    <header className="navbar shadow-sm bg-primary text-white">
-      <div className="flex-1">
-        <Link to="/home/dashboard" style={{ marginRight: 10 }}>
+    <header className="navbar shadow-sm">
+      <div className="flex-1 tabs">
+        <NavLink
+          className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
+          to="/home/dashboard"
+          style={{ marginRight: 10 }}
+        >
           Dashboard
-        </Link>
-        <Link to="/home/tenant">Tenants</Link>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
+          to="/home/tenant"
+        >
+          Tenants
+        </NavLink>
       </div>
       <div className="flex-none">
         <button className="cursor-pointer" onClick={logout}>
